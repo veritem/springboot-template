ccbedab7e82aa7a892e608eb64a74e1bc00c3556
Makuza Mugabo Verite <53856673+makuzaverite@users.noreply.github.com>
2021-03-28 10:54:22 +0200

    first commit

---
+++ src/main/java/com/example/template/TemplateApplication.java
@@ -0,0 +1,13 @@
+package com.example.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemplateApplication.class, args);
+	}
+
+}
